0531f6fbe1ce7c79dae715cf17c75259dcc92d5b
Leonardo Pastrana <leonardo.pastrana@wizeline.com>
2020-08-07 02:33:35 -0500

    [IMP] Animated NoteList when adding removing and archiving

--- lib/ui/note_list.dart
+++ lib/ui/note_list.dart
@@ -13,6 +13,7 @@ class NoteList extends StatefulWidget {
 }
 
 class _NoteListState extends State<NoteList> {
+  final GlobalKey<AnimatedListState> _animatedListKey = GlobalKey();
   final _random = new Random();
 
   @override
@@ -22,16 +23,24 @@ class _NoteListState extends State<NoteList> {
         color: CustomColors.grayPrimaryColor,
         child: Consumer<NotesModel>(
           builder: (context, notesModel, child) {
-            return ListView.builder(
-              itemBuilder: (context, index) {
-                return NoteWidget(
-                  note: notesModel.noteList.elementAt(index),
-                  onArchivePressed: () => notesModel.archiveNote(index),
-                  onDeletePressed: () => notesModel.deleteNote(index),
-                );
-              },
-              itemCount: notesModel.noteList.length,
-            );
+            return AnimatedList(
+                key: _animatedListKey,
+                initialItemCount: notesModel.noteList.length,
+                itemBuilder: (context, index, animation) {
+                  final currentNote = notesModel.noteList.elementAt(index);
+                  return _buildNewNoteWidget(
+                    currentNote,
+                    animation,
+                    onArchivePressed: () {
+                      notesModel.archiveNote(index);
+                      _animateNoteArchived(currentNote, index);
+                    },
+                    onDeletePressed: () {
+                      notesModel.deleteNote(index);
+                      _animateNoteDeleted(currentNote, index);
+                    },
+                  );
+                });
           },
         ),
       ),
@@ -42,18 +51,70 @@ class _NoteListState extends State<NoteList> {
           color: Colors.white70,
         ),
         onPressed: () {
+          var notesModel = Provider.of<NotesModel>(context, listen: false);
           setState(() {
-            Provider.of<NotesModel>(context, listen: false).addNote(
+            notesModel.addNote(
               Note(
                 title: 'This is a new title! :)',
                 body:
                     'This is the body of a test note that I am making, I hope it works as good as I expect it to work',
-                color: CustomColors.noteColors[_random.nextInt(CustomColors.noteColors.length)],
+                color: CustomColors.noteColors[
+                    _random.nextInt(CustomColors.noteColors.length)],
               ),
             );
           });
+          _animatedListKey.currentState
+              .insertItem(notesModel.noteList.length - 1);
         },
       ),
     );
+  }
+
+  Widget _buildNewNoteWidget(Note note, Animation animation,
+      {Function() onDeletePressed, Function() onArchivePressed}) {
+    return SizeTransition(
+      sizeFactor: animation,
+      child: NoteWidget(
+        note: note,
+        onArchivePressed: onArchivePressed,
+        onDeletePressed: onDeletePressed,
+      ),
+    );
+  }
+
+  Widget _buildDeletedNoteWidget(Note note, Animation animation) {
+    return SizeTransition(
+      sizeFactor: animation,
+      child: NoteWidget(
+        note: note,
+        onArchivePressed: () => {},
+        onDeletePressed: () => {},
+      ),
+    );
+  }
+
+  Widget _buildArchivedNoteWidget(Note note, Animation animation) {
+    return ScaleTransition(
+      scale: animation,
+      child: NoteWidget(
+        note: note,
+        onArchivePressed: () => {},
+        onDeletePressed: () => {},
+      ),
+    );
+  }
+
+  void _animateNoteDeleted(Note note, int index) {
+    AnimatedListRemovedItemBuilder builder = (context, animation) {
+      return _buildDeletedNoteWidget(note, animation);
+    };
+    _animatedListKey.currentState.removeItem(index, builder);
+  }
+
+  void _animateNoteArchived(Note note, int index) {
+    AnimatedListRemovedItemBuilder builder = (context, animation) {
+      return _buildArchivedNoteWidget(note, animation);
+    };
+    _animatedListKey.currentState.removeItem(index, builder);
   }
 }
