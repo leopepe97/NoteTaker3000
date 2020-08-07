a12c490a778911f54a933742f72227ef369dd250
Leonardo Pastrana <leonardo.pastrana@wizeline.com>
2020-08-07 04:04:44 -0500

    [IMP] Added toMap and fromMap to Note class

--- lib/model/note.dart
+++ lib/model/note.dart
@@ -8,6 +8,26 @@ class Note {
   final Color color;
   bool isArchived;
 
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'title': title,
+      'body': body,
+      'color': color.value,
+      'isArchived': isArchived,
+    };
+  }
+
+  static Note fromMap(Map<String, dynamic> map) {
+    return Note(
+      id: map['id'],
+      title: map['title'],
+      body: map['body'],
+      color: Color(map['color']),
+      isArchived: map['isArchived'],
+    );
+  }
+
   Note({this.id, this.title, this.body, this.color, this.isArchived = false});
 
 }
